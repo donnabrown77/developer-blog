d56b8e710bafcdcbfecfbafbe43e93c2a9c6ddea
dbrown77 <donna.brown05@gmail.com>
2023-08-21 12:38:08 -0700

    remove unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,6 @@ const linkHeadingStyles = {
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
